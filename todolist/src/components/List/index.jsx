c5348f29675882bcaa5bf1bb4011f3cf23bf0e41
shisanzhoufu <1223945508@qq.com>
2021-02-08 22:10:41 +0800

    todoList

--- todolist/src/components/List/index.jsx
+++ todolist/src/components/List/index.jsx
@@ -1,14 +1,21 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import Item from '../Item'
 import './index.css'
 export default class List extends Component {
+    //对接收的数据进行限制
+    static propTypes = {
+        todos:PropTypes.array.isRequired,
+        updateTodo:PropTypes.func.isRequired,
+        deleteTodo:PropTypes.func.isRequired
+    }
     render() {
-        const {todos} = this.props
+        const {todos,updateTodo,deleteTodo} = this.props
         return (
             <ul className="todo-main"> 
             {
                 todos.map((todos) => {
-                    return <Item key={todos.id} {...todos}/>
+                    return <Item key={todos.id} {...todos} updateTodo={updateTodo} deleteTodo={deleteTodo}/>
                 })
                 
             }
