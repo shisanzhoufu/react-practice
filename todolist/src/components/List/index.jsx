ead14071b1ff26ff735f53c1b7eb0491af428540
shisanzhoufu <1223945508@qq.com>
2021-02-10 21:53:39 +0800

    GitHub搜索案例

--- todolist/src/components/List/index.jsx
+++ todolist/src/components/List/index.jsx
@@ -1,25 +1,40 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-import Item from '../Item'
 import './index.css'
 export default class List extends Component {
-    //对接收的数据进行限制
-    static propTypes = {
-        todos:PropTypes.array.isRequired,
-        updateTodo:PropTypes.func.isRequired,
-        deleteTodo:PropTypes.func.isRequired
-    }
     render() {
-        const {todos,updateTodo,deleteTodo} = this.props
         return (
-            <ul className="todo-main"> 
-            {
-                todos.map((todos) => {
-                    return <Item key={todos.id} {...todos} updateTodo={updateTodo} deleteTodo={deleteTodo}/>
-                })
-                
-            }
-            </ul>
+            <div className="row">
+            <div className="card">
+              <a href="https://github.com/reactjs" target="_blank" rel="noreferrer">
+                <img alt="head_avator"  src="https://avatars.githubusercontent.com/u/6412038?v=3" style={{width:'100px'}}/>
+              </a>
+              <p className="card-text">reactjs</p>
+            </div>
+            <div className="card">
+              <a href="https://github.com/reactjs" target="_blank"  rel="noreferrer">
+                <img alt="head_avator" src="https://avatars.githubusercontent.com/u/6412038?v=3" style={{width:'100px'}}/>
+              </a>
+              <p className="card-text">reactjs</p>
+            </div>
+            <div className="card">
+              <a href="https://github.com/reactjs" target="_blank" rel="noreferrer">
+                <img alt="head_avator" src="https://avatars.githubusercontent.com/u/6412038?v=3" style={{width:'100px'}}/>
+              </a>
+              <p className="card-text">reactjs</p>
+            </div>
+            <div className="card">
+              <a href="https://github.com/reactjs" target="_blank" rel="noreferrer">
+                <img alt="head_avator" src="https://avatars.githubusercontent.com/u/6412038?v=3" style={{width:'100px'}}/>
+              </a>
+              <p className="card-text">reactjs</p>
+            </div>
+            <div className="card">
+              <a href="https://github.com/reactjs" target="_blank" rel="noreferrer">
+                <img alt="head_avator" src="https://avatars.githubusercontent.com/u/6412038?v=3" style={{width:'100px'}}/>
+              </a>
+              <p className="card-text">reactjs</p>
+            </div>
+          </div>
         )
     }
 }
--- todolist/src/index.js
+++ todolist/src/index.js
@@ -3,6 +3,6 @@ import React from 'react'
 //引入ReactDOM
 import ReactDOM from 'react-dom'
 //引入App
-import App from './APP'
+import App from './App'
 
 ReactDOM.render(<App/>,document.getElementById('root'))
