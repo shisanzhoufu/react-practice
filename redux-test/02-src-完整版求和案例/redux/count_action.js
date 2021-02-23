d6d42793f61f060822a242aad0773fb4903215af
shisanzhoufu <1223945508@qq.com>
2021-02-23 14:57:02 +0800

    完整版求和案例

---
+++ redux-test/02-src-完整版求和案例/redux/count_action.js
@@ -0,0 +1,7 @@
+/**
+ * 生成action对象
+ */
+import {INCREMENT,DECREMENT} from './constant'
+export const createIncrementAction = data => ({type:INCREMENT,data})
+export const createDecrementAction = data =>({type:DECREMENT,data})
+ 
