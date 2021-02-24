7046e7bc48c011b27d9244af016dcdec6f8cda93
shisanzhoufu <1223945508@qq.com>
2021-02-24 10:31:16 +0800

    react-redux精简合并版

--- redux-test/03-src-react_redux基础版/index.js
+++ redux-test/03-src-react_redux基础版/index.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import {Provider} from 'react-redux'
 import store from './redux/store'
 
-ReactDOM.render(<App/>,document.getElementById('root'))
+ReactDOM.render(
+	<Provider store={store}>
+		<App/>
+	</Provider>,
+document.getElementById('root')
+)
 
-store.subscribe(()=>{
-	ReactDOM.render(<App/>,document.getElementById('root'))
-})
+//使用了react-redux酒不用再检测了
+// store.subscribe(()=>{
+// 	ReactDOM.render(<App/>,document.getElementById('root'))
+// })
--- redux-test/04-src-react_redux合并精简版/App.jsx
+++ redux-test/04-src-react_redux合并精简版/App.jsx
@@ -1,13 +1,11 @@
 import React, { Component } from 'react'
 import Count from './contaniers/Count'
-
-//通过index.js provider传动store
-// import store from './redux/store'
+import store from './redux/store'
 export default class App extends Component {
 	render() {
 		return (
 			<div>
-				<Count/>
+				<Count store={store}/>
 			</div>
 		)
 	}
