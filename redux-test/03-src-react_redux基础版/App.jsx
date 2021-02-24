a0cbbd0acbfbc42b93eb82f3a8c7616857128fa1
shisanzhoufu <1223945508@qq.com>
2021-02-24 09:02:40 +0800

    react-redux 基础版

--- redux-test/03-src-react_redux基础版/App.jsx
+++ redux-test/03-src-react_redux基础版/App.jsx
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
-import Count from './components/Count'
-
+import Count from './contaniers/Count'
+import store from './redux/store'
 export default class App extends Component {
 	render() {
 		return (
 			<div>
-				<Count/>
+				<Count store={store}/>
 			</div>
 		)
 	}
